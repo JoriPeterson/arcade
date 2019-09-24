eb0e7bf1c768dc561177b79c69f13ab2330179cc
Jori Peterson <joripeterson@gmail.com>
2019-09-24 14:40:11 -0600

    Update games for post request

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -14,16 +14,37 @@ router.get("/", function(req, res, next) {
   });
 });
 
-// router.get("/:id"), function(req, res, next) {
-//   Game.findByID()
-//   .then(games => {
-//     res.setHeader("Content-Type", "application/json");
-//     res.status(200).send(JSON.stringify(games));
-//   })
-//   .catch(error => {
-//     res.setHeader("Content-Type", "application/json");
-//     res.status(500).send({error})
-//   });
-// });
+router.get("/:id"), function(req, res, next) {
+  Game.findAll({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(games => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify(games));
+  })
+  .catch(error => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(500).send({error})
+  });
+});
+
+router.post("/", function(req, res, next) {
+  Game.create({
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+    })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({ error });
+    });
+});
 
 module.exports = router;
